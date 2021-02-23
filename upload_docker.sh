54446505e492f0366a748f4ca340fed6fd06837a
assemelgendy <assem.elgendy@gmail.com>
2021-02-23 13:06:33 +0200

    first commit

---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=assemelgendy/flask
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login -u assemelgendy
+docker tag flask $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
